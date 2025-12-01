120eb1e43354a075ad9512af9332a77f91ebc9cb
RaulArgenteCortes <raulargentecortes@gmail.com>
2025-12-01 17:26:12 +0100

    Mesh fully coded

--- HookGame_Project/Assets/_HookGame/Scripts/Player/PlayerMeshController.cs
+++ HookGame_Project/Assets/_HookGame/Scripts/Player/PlayerMeshController.cs
@@ -13,13 +13,14 @@ public class PlayerMeshController : MonoBehaviour
     [SerializeField] GameObject connector;
 
     [Header("Transform stats")]
-    [SerializeField] float rotateInput;
+    [SerializeField] float horizontalInput;
     [SerializeField] Rigidbody wheelRB;
+    [SerializeField] float hookAngle;
 
     [Header("Script references")]
     [SerializeField] PlayerController playerController;
 
-    #region Update Functions
+    #region Awake/Start Functions
     private void Start()
     {
         playerController = GetComponent<PlayerController>();
@@ -36,32 +37,69 @@ public class PlayerMeshController : MonoBehaviour
         DistanceChange();
 
         DirectionChange();
+
+        LookChange();
     }
     
     private void InputChange()
     {
-        rotateInput = Mathf.MoveTowards(
-            rotateInput,
-            playerController.moveInput.x / 2,
+        // Sets the player's horizontal input whit a transition.
+        horizontalInput = Mathf.MoveTowards(
+            horizontalInput,
+            playerController.moveInput.x,
             0.1f * Time.fixedDeltaTime
         );
 
-        disc3.transform.localEulerAngles = new Vector3(0, disc3.transform.localEulerAngles.y + rotateInput, 0);
+        // Rotates the disc3 in relation to the speed.
+        disc3.transform.localEulerAngles = new Vector3(0, disc3.transform.localEulerAngles.y + (horizontalInput / 2), 0);
     }
 
     private void SpeedChange()
     {
+        // Rotates the wheel in relation to the speed.
         wheel.transform.localEulerAngles = new Vector3(0, wheel.transform.localEulerAngles.y + (playerController.currentSpeed*2), 0);
     }
 
     private void DistanceChange()
     {
+        // Rotates the disc2 in relation to the distance from the wheel.
         disc2.transform.localEulerAngles = new Vector3(0, wheelRB.transform.localPosition.y * 180, 0);
+
+        // Rescales the connector in relation to the distance from the wheel.
+        connector.transform.localScale = new Vector3(100, 100, 100 * wheelRB.transform.localPosition.y);
     }
 
     private void DirectionChange()
     {
-        //eyeA.transform.localEulerAngles = new Vector3(0, wheelRB.transform.localPosition.y * 180, 0);
+        // Sets the hook rotation.
+        if (playerController.moveInput != Vector2.zero)
+        {
+            hookAngle = Snapping.Snap(
+                -Mathf.Atan2(-playerController.moveInput.x, playerController.moveInput.y) * Mathf.Rad2Deg,
+                45
+            );
+        }
+
+        // Rotates the disc1 in relation to the hook rotation.
+        disc1.transform.localRotation = Quaternion.RotateTowards(
+            disc1.transform.localRotation,
+            Quaternion.Euler(new Vector3(0, hookAngle, 0)),
+            360 * 2 * Time.deltaTime
+        );
+    }
+
+    private void LookChange()
+    {
+        // Rotates the eyeA in relation to the player input.
+        eyeA.transform.localRotation = Quaternion.RotateTowards(
+            eyeA.transform.localRotation,
+            Quaternion.Euler(new Vector3(
+                playerController.moveInput.y * 30,
+                0,
+                -playerController.moveInput.x * 30 + 180
+            )),
+            360 * Time.deltaTime
+        );
 
         // Also changes the other eye.
         eyeB.transform.localEulerAngles = eyeA.transform.localEulerAngles;
